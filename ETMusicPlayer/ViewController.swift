54cb0847ef284cfc7c178931f5aa7eeb45d4178b
李芫萱 <yuansyuanli@liyanxuandebijixingdiannao.local>
2024-06-29 16:39:41 +0800

    first commit

---
+++ ETMusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ETMusicPlayer
+//
+//  Created by 李芫萱 on 2024/6/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
